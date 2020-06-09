ae174b43887d1e7fd070f38968264a50769b2b97
Gennadiy <Relect@bk.ru>
2020-06-09 18:25:45 +0300

    Exception in thread "main" java.lang.NullPointerException 	at Server.main(Server.java:46) 	из за localhost/api в строке 17

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,34 +12,39 @@ public class Server {
 
     public static void main(String[] args) {
         try {
-            try {
-                server = new ServerSocket(4004); // серверсокет прослушивает порт 4004
-                System.out.println("Сервер запущен!"); // хорошо бы серверу
-                //   объявить о своем запуске
-                clientSocket = server.accept(); // accept() будет ждать пока
-                //кто-нибудь не захочет подключиться
-                try { // установив связь и воссоздав сокет для общения с клиентом можно перейти
-                    // к созданию потоков ввода/вывода.
-                    // теперь мы можем принимать сообщения
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    // и отправлять
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            while (true) {
+                try {
+                    server = new ServerSocket(4004, 10, InetAddress.getByName("localhost/api")); // серверсокет прослушивает порт 4004
+                    System.out.println("Сервер запущен!"); // хорошо бы серверу
+                    //   объявить о своем запуске
+                    clientSocket = server.accept(); // accept() будет ждать пока
+                    //кто-нибудь не захочет подключиться
+                    try { // установив связь и воссоздав сокет для общения с клиентом можно перейти
+                        // к созданию потоков ввода/вывода.
+                        // теперь мы можем принимать сообщения
+                        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        // и отправлять
+                        out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                    String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
-                    System.out.println(word);
-                    // не долго думая отвечает клиенту
-                    out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
-                    out.flush(); // выталкиваем все из буфера
+                        String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
+                        System.out.println(word);
+                        // не долго думая отвечает клиенту
+                        out.write("HTTP/1.1 200 OK");
+                        out.write("Content-Type: text/html; charset=utf-8");
+                        out.write("");
+                        out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
+                        out.flush(); // выталкиваем все из буфера
 
-                } finally { // в любом случае сокет будет закрыт
-                    clientSocket.close();
-                    // потоки тоже хорошо бы закрыть
-                    in.close();
-                    out.close();
+                    } finally { // в любом случае сокет будет закрыт
+                        clientSocket.close();
+                        // потоки тоже хорошо бы закрыть
+                        in.close();
+                        out.close();
+                    }
+                } finally {
+                    System.out.println("Сервер закрыт!");
+                    server.close();
                 }
-            } finally {
-                System.out.println("Сервер закрыт!");
-                server.close();
             }
         } catch (IOException e) {
             System.err.println(e);
